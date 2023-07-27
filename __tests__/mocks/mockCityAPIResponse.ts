036e222e61cfc06bc5c35e672ae566330cf7c0ab
Iann Rodrigues <iannrst11@gmail.com>
2023-07-27 16:06:54 -0300

    feat: Criando pasta para mocks

---
+++ __tests__/mocks/mockCityAPIResponse.ts
@@ -0,0 +1,14 @@
+import { CityAPIResponse } from '@services/getCityByNameService';
+
+export const mockCityAPIResponse: CityAPIResponse = {
+  id: '1',
+  name: 'Barreiras',
+  sys: {
+    country: 'BR'
+  },
+  coord: {
+    lat: 123,
+    lon: 456
+  }
+
+}
--- src/services/getCityByNameService.spec.ts
+++ src/services/getCityByNameService.spec.ts
@@ -1,21 +1,11 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResponse";
 import { api } from "./api";
 import { getCityByNameService } from "./getCityByNameService";
 
 describe("API: getCityByNameService", ()=>{
   it("should return city details", async ()=>{
-    const data = {
-      id: '1',
-      name: 'Barreiras',
-      sys: {
-        country: 'BR'
-      },
-      coord: {
-        lat: 123,
-        lon: 456
-      }
-    };
 
-    jest.spyOn(api, "get").mockResolvedValue({data}); // fica observando as requisições feita na api
+    jest.spyOn(api, "get").mockResolvedValue({data: mockCityAPIResponse}); // fica observando as requisições feita na api
     const response = await getCityByNameService('Barreiras')
 
    expect(response.length).toBeGreaterThan(0);
